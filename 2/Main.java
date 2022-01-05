caafbf69adb0f1a2b712f7917e219a60a91918b3
mhemanthkmr <m.hemanthkmr143@gmail.com>
2022-01-05 17:42:34 +0530

    2 Folder Main.java is Odd or Even Program

---
+++ 2/Main.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        System.out.println("Odd Numbers");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+        System.out.println("Even Numbers");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
